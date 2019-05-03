4d002e5a6a3e8ecb733ea6b52ed65771c753beba
Witold Kupś <witold.kups@interlan.pl>
2019-05-03 16:10:13 +0200

    Extract group edit to component

--- src/views/dashboard/DashboardGroup.vue
+++ src/views/dashboard/DashboardGroup.vue
@@ -2,25 +2,7 @@
     <v-card class="mx-auto" :color="group && group.color" max-width="400">
         <v-card-title>
             <template v-if="edit">
-                <v-form ref="form" v-model="valid" lazy-validation @submit.prevent="saveChanges"
-                        class="full-width title font-weight-light">
-                    <v-layout row wrap align-center>
-                        <v-flex>
-                            <v-text-field
-                                    label="Name" v-model="newGroup.name" single-line full-width
-                                    hide-details :rules="nameRules" class="text-xs-center"></v-text-field>
-                        </v-flex>
-                        <v-btn icon flat type="submit" :loading="loading" :disabled="loading || !valid">
-                            <v-icon>save</v-icon>
-                            <span slot="loader" class="custom-loader">
-                                <v-icon light>cached</v-icon>
-                            </span>
-                        </v-btn>
-                        <v-btn icon flat @click.stop="revert">
-                            <v-icon>undo</v-icon>
-                        </v-btn>
-                    </v-layout>
-                </v-form>
+                <edit-group :group="newGroup" @finished="onEditFinish"></edit-group>
             </template>
             <template v-else>
                 <v-flex>
@@ -38,43 +20,27 @@
     </v-card>
 </template>
 <script>
-    import {mapGetters} from "vuex";
+    import EditGroup from "./EditGroup";
 
     export default {
+        components: {EditGroup},
         props: {
             group: null,
             groupId: ''
         },
         data() {
             return {
-                valid: false,
                 edit: !this.group,
-                newGroup: null,
-                nameRules: [
-                    v => !!v || "Name cannot be empty",
-                    v => v.length <= 20 || "Name must be no longer than 20 characters"
-                ]
+                newGroup: null
             }
-        },
-        computed: {
-            ...mapGetters(['loading']),
         },
         methods: {
             startEdit() {
-                this.newGroup = Object.assign({}, this.group);
+                this.newGroup = Object.assign({groupId: this.groupId}, this.group);
                 this.edit = true;
             },
-            saveChanges() {
-                if (!this.$refs.form.validate()) return;
-                const changed = this.group.name !== this.newGroup.name;
-                if (changed)
-                    this.$store.dispatch('updateDashboardGroup', {groupId: this.groupId, ...this.newGroup})
-                        .then(v => this.edit = false);
-                else
-                    this.edit = false;
-            },
-            revert() {
-                this.edit = false
+            onEditFinish() {
+                this.edit = false;
             }
         }
     }
---
+++ src/views/dashboard/EditGroup.vue
@@ -0,0 +1,62 @@
+<template>
+    <v-form ref="form" v-model="valid" lazy-validation @submit.prevent="saveChanges"
+            class="full-width title font-weight-light">
+        <v-layout row wrap align-center>
+            <v-flex>
+                <v-text-field
+                        label="Name" v-model="name" single-line full-width
+                        hide-details :rules="nameRules" class="text-xs-center"></v-text-field>
+            </v-flex>
+            <v-btn icon flat type="submit" :loading="loading" :disabled="loading || !valid">
+                <v-icon>save</v-icon>
+                <span slot="loader" class="custom-loader">
+                                <v-icon light>cached</v-icon>
+                            </span>
+            </v-btn>
+            <v-btn icon flat @click.stop="finish">
+                <v-icon>undo</v-icon>
+            </v-btn>
+        </v-layout>
+    </v-form>
+</template>
+
+<script>
+    import {mapGetters} from "vuex";
+
+    export default {
+        name: "EditGroup",
+        props: ['group'],
+        data() {
+            return {
+                valid: false,
+                name: this.group.name,
+                color: this.group.color,
+                nameRules: [
+                    v => !!v || "Name cannot be empty",
+                    v => v.length <= 20 || "Name must be no longer than 20 characters"
+                ]
+            }
+        },
+        computed: mapGetters(["loading"]),
+        methods: {
+            saveChanges() {
+                if (this.group.name !== this.name)
+                    this.$store.dispatch('updateDashboardGroup', {
+                        groupId: this.group.groupId,
+                        name: this.name,
+                        color: this.color
+                    })
+                        .then(() => this.finish());
+                else
+                    this.finish()
+            },
+            finish() {
+                this.$emit('finished')
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
