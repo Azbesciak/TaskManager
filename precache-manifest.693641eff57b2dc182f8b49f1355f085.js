326685f5fb4d6481e42627969abd68885c405e89
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-03 12:36:32 +0000

    Deploy Azbesciak/TaskManager to github.com/Azbesciak/TaskManager.git:gh-pages

---
+++ precache-manifest.693641eff57b2dc182f8b49f1355f085.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9ca0e3208e6651ded22",
+    "url": "/TaskManager/css/app.284d8178.css"
+  },
+  {
+    "revision": "b9ca0e3208e6651ded22",
+    "url": "/TaskManager/js/app.a05d69b5.js"
+  },
+  {
+    "revision": "c56abe22452c660a50ba",
+    "url": "/TaskManager/css/chunk-vendors.411bb395.css"
+  },
+  {
+    "revision": "c56abe22452c660a50ba",
+    "url": "/TaskManager/js/chunk-vendors.8196becc.js"
+  },
+  {
+    "revision": "93bf47560e08297c6a50",
+    "url": "/TaskManager/css/dashboard.03e83b9c.css"
+  },
+  {
+    "revision": "93bf47560e08297c6a50",
+    "url": "/TaskManager/js/dashboard.6bc1a9b1.js"
+  },
+  {
+    "revision": "5a496d5159d6ba7e3605",
+    "url": "/TaskManager/js/dashboards.8abc1c4c.js"
+  },
+  {
+    "revision": "b6f7e7aa85596a4b4bd4",
+    "url": "/TaskManager/js/login.713d73e1.js"
+  },
+  {
+    "revision": "2b677dff2e20291192b4",
+    "url": "/TaskManager/js/signUp.de4a1046.js"
+  },
+  {
+    "revision": "e6257a726a0cf6ec8c6fec22821c055f",
+    "url": "/TaskManager/fonts/fa-regular-400.e6257a72.woff2"
+  },
+  {
+    "revision": "427d721b86fc9c68b2e85ad42b69238c",
+    "url": "/TaskManager/fonts/fa-regular-400.427d721b.woff"
+  },
+  {
+    "revision": "dcce4b7fbd5e895561e18af4668265af",
+    "url": "/TaskManager/fonts/fa-regular-400.dcce4b7f.eot"
+  },
+  {
+    "revision": "65b9977aa23185e8964b36eddbce7a20",
+    "url": "/TaskManager/fonts/fa-regular-400.65b9977a.ttf"
+  },
+  {
+    "revision": "5e2f92123d241cabecf0b289b9b08d4a",
+    "url": "/TaskManager/fonts/fa-brands-400.5e2f9212.woff2"
+  },
+  {
+    "revision": "418dad87601f9c8abd0e5798c0dc1feb",
+    "url": "/TaskManager/fonts/fa-solid-900.418dad87.woff2"
+  },
+  {
+    "revision": "2ef8ba3410dcc71578a880e7064acd7a",
+    "url": "/TaskManager/fonts/fa-brands-400.2ef8ba34.woff"
+  },
+  {
+    "revision": "a7140145ebaaf5fb14e40430af5d25c4",
+    "url": "/TaskManager/fonts/fa-solid-900.a7140145.woff"
+  },
+  {
+    "revision": "a7b95dbdd87e0c809570affaf366a434",
+    "url": "/TaskManager/fonts/fa-brands-400.a7b95dbd.eot"
+  },
+  {
+    "revision": "98b6db59be947f563350d2284fc9ea36",
+    "url": "/TaskManager/fonts/fa-brands-400.98b6db59.ttf"
+  },
+  {
+    "revision": "5eb754ab7dbd2fee562360528db4c3c0",
+    "url": "/TaskManager/img/fa-regular-400.5eb754ab.svg"
+  },
+  {
+    "revision": "ff8d9f8adb0d09f11d4816a152672f53",
+    "url": "/TaskManager/fonts/fa-solid-900.ff8d9f8a.ttf"
+  },
+  {
+    "revision": "46e7cec623d8bd790d9fdbc8de2d3ee7",
+    "url": "/TaskManager/fonts/fa-solid-900.46e7cec6.eot"
+  },
+  {
+    "revision": "5bf145531213545e03ff41cd27df7d2b",
+    "url": "/TaskManager/img/fa-brands-400.5bf14553.svg"
+  },
+  {
+    "revision": "49279363201ed19a840796e05a74a91b",
+    "url": "/TaskManager/img/fa-solid-900.49279363.svg"
+  },
+  {
+    "revision": "5fbf14ba8086ab64b8e331496b0079fe",
+    "url": "/TaskManager/index.html"
+  },
+  {
+    "revision": "a291c7687bff0f1cc7c781bf38d3678f",
+    "url": "/TaskManager/favicon.png"
+  },
+  {
+    "revision": "f80fab9d8dc45eaa6d9d8f3afe4bb2d0",
+    "url": "/TaskManager/img/icon-main.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/TaskManager/robots.txt"
+  }
+];
